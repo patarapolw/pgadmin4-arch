fa800afe1e2eb8ae659ff238953e470fd61860fc
Pacharapol Withayasakpunt <polv@polv.cc>
2022-02-12 08:43:39 +0700

    add attempt to update pgadmin4

--- make.sh
+++ make.sh
@@ -1,11 +1,25 @@
 #!/bin/bash -e
 
-docker build -t pgadmin4-arch .
-mkdir -p ./out
-docker run -v $(pwd)/out:/out pgadmin4-arch
+docker build --no-cache -t pgadmin4-arch .
 
-cd out
+DEB="./deb"
+PKG="./pkg"
+
+rm -rf "$DEB"
+mkdir -p "$DEB"
+
+mkdir -p "$PKG"
+
+docker run -v "$(pwd)/$DEB:/out" pgadmin4-arch
+
+cd "$DEB"
 for i in $(find . -maxdepth 1 -type f -iname 'pgadmin4-*.deb')
 do
+    echo "$i"
     debtap "$i"
+done
+
+for i in $(find . -maxdepth 1 -type f -iname 'pgadmin4-*.pkg.tar.zst')
+do
+    mv "$i" "../$PKG/$i"
 done
